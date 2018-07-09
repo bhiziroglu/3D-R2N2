c45f6c538322d439d51bc95aa51255894331114a
Berkan <bhiziroglu14@ku.edu.tr>
2018-07-09 18:35:58 +0300

    Decoder changed to paper architecture.

--- net.py
+++ net.py
@@ -6,6 +6,7 @@ import dataset
 from tqdm import tqdm
 #from tensorflow.python import debug as tf_debug
 
+
 n_convfilter = [96, 128, 256, 256, 256, 256]
 n_deconvfilter = [128, 128, 128, 64, 32, 2]
 n_gru_vox = 4
@@ -53,17 +54,17 @@ encoder_gru_kernel_shapes = [
 ]
 
 decoder_kernel_shapes = [
-    [3,3,3,n_deconvfilter[1],n_deconvfilter[1]], #conv7a
-    [3,3,3,n_deconvfilter[1],n_deconvfilter[1]], #conv7b
-    [3,3,3,n_deconvfilter[1],n_deconvfilter[2]], #conv8a
-    [3,3,3,n_deconvfilter[2],n_deconvfilter[2]], #conv8b
-    [3,3,3,n_deconvfilter[2],n_deconvfilter[3]], #conv9a
-    [3,3,3,n_deconvfilter[3],n_deconvfilter[3]], #conv9b
-    [1,1,1,n_deconvfilter[2],n_deconvfilter[3]], #conv9c
-    [3,3,3,n_deconvfilter[3],n_deconvfilter[4]], #conv10a
-    [3,3,3,n_deconvfilter[4],n_deconvfilter[4]], #conv10b
-    [3,3,3,n_deconvfilter[4],n_deconvfilter[4]], #conv10c
-    [3,3,3,n_deconvfilter[4],n_deconvfilter[5]] #conv11a
+    [3,3,3,n_deconvfilter[1],n_deconvfilter[1]], #conv7a #0
+    [3,3,3,n_deconvfilter[1],n_deconvfilter[1]], #conv7b #1
+    [3,3,3,n_deconvfilter[1],n_deconvfilter[2]], #conv8a #2
+    [3,3,3,n_deconvfilter[2],n_deconvfilter[2]], #conv8b #3
+    [3,3,3,n_deconvfilter[2],n_deconvfilter[3]], #conv9a #4
+    [3,3,3,n_deconvfilter[3],n_deconvfilter[3]], #conv9b #5
+    [1,1,1,n_deconvfilter[2],n_deconvfilter[3]], #conv9c #6
+    [3,3,3,n_deconvfilter[3],n_deconvfilter[4]], #conv10a#7
+    [3,3,3,n_deconvfilter[4],n_deconvfilter[4]], #conv10b#8
+    [3,3,3,n_deconvfilter[4],n_deconvfilter[4]], #conv10c#9
+    [3,3,3,n_deconvfilter[4],n_deconvfilter[5]] #conv11a #10
 ]
 
 
@@ -73,70 +74,70 @@ def build_graph():
 
     with tf.name_scope("encoder_gru_weights"):
         w = [tf.get_variable(
-            "w"+str(_), shape=kernel, initializer = tf.glorot_normal_initializer(seed=666), trainable=True) 
+            "w"+str(_), shape=kernel, initializer = tf.glorot_normal_initializer(seed=1231), trainable=True) 
             for _,kernel in enumerate(encoder_gru_kernel_shapes)]
 
     with tf.name_scope("decoder_weights"):
         w_decoder = [tf.get_variable(
-            "w2_"+str(_), shape=kernel, initializer = tf.glorot_normal_initializer(seed=6666), trainable=True) 
+            "w2_"+str(_), shape=kernel, initializer = tf.glorot_normal_initializer(seed=1231), trainable=True) 
             for _,kernel in enumerate(decoder_kernel_shapes)]
 
     with tf.name_scope("Encoder"):
         
         # Convolutional Layer #1
         conv1a = tf.nn.conv2d(input=X,filter=w[0],strides=[1,1,1,1],padding="SAME")
-        conv1a = tf.nn.leaky_relu(conv1a)
+        conv1a = tf.nn.leaky_relu(conv1a,alpha=0.01)
         conv1b = tf.nn.conv2d(input=conv1a,filter=w[1],strides=[1,1,1,1],padding="SAME")
-        conv1b = tf.nn.leaky_relu(conv1b)
+        conv1b = tf.nn.leaky_relu(conv1b,alpha=0.01)
         pool1 = tf.nn.max_pool(conv1b,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
         # [1, 64, 64, 96]
 
         # Convolutional Layer #2
         conv2a = tf.nn.conv2d(input=pool1,filter=w[2],strides=[1,1,1,1],padding="SAME")
-        conv2a = tf.nn.leaky_relu(conv2a)
+        conv2a = tf.nn.leaky_relu(conv2a,alpha=0.01)
         conv2b = tf.nn.conv2d(input=conv2a,filter=w[3],strides=[1,1,1,1],padding="SAME")
-        conv2b = tf.nn.leaky_relu(conv2b)
+        conv2b = tf.nn.leaky_relu(conv2b,alpha=0.01)
         conv2c = tf.nn.conv2d(input=pool1,filter=w[4],strides=[1,1,1,1],padding="SAME")
-        conv2c = tf.nn.leaky_relu(conv2c)
+        conv2c = tf.nn.leaky_relu(conv2c,alpha=0.01)
         res2 = tf.add(conv2b,conv2c)
         pool2 = tf.nn.max_pool(res2,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
         ''' !!!TODO!!!  (1, 32, 32, 128)   ->>>      Paper result size is (1, 33, 33, 128)'''
 
         # Convolutional Layer #3
         conv3a = tf.nn.conv2d(input=pool2,filter=w[5],strides=[1,1,1,1],padding="SAME")
-        conv3a = tf.nn.leaky_relu(conv3a)
+        conv3a = tf.nn.leaky_relu(conv3a,alpha=0.01)
         conv3b = tf.nn.conv2d(input=conv3a,filter=w[6],strides=[1,1,1,1],padding="SAME")
-        conv3b = tf.nn.leaky_relu(conv3b)
+        conv3b = tf.nn.leaky_relu(conv3b,alpha=0.01)
         conv3c = tf.nn.conv2d(input=pool2,filter=w[7],strides=[1,1,1,1],padding="SAME")
-        conv3c = tf.nn.leaky_relu(conv3c)
+        conv3c = tf.nn.leaky_relu(conv3c,alpha=0.01)
         res3 = tf.add(conv3b,conv3c)
         pool3 = tf.nn.max_pool(res3,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
         ''' !!!TODO!!!  (1, 16, 16, 256)   ->>>      Paper result size is (1, 17, 17, 256)'''
 
         # Convolutional Layer #4
         conv4a = tf.nn.conv2d(input=pool3,filter=w[8],strides=[1,1,1,1],padding="SAME")
-        conv4a = tf.nn.leaky_relu(conv4a)
+        conv4a = tf.nn.leaky_relu(conv4a,alpha=0.01)
         conv4b = tf.nn.conv2d(input=conv4a,filter=w[9],strides=[1,1,1,1],padding="SAME")
-        conv4b = tf.nn.leaky_relu(conv4b)
+        conv4b = tf.nn.leaky_relu(conv4b,alpha=0.01)
         pool4 = tf.nn.max_pool(conv4b,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
         ''' !!!TODO!!!  (1, 8, 8, 256)   ->>>      Paper result size is (1, 9, 9, 256)'''
     
         # Convolutional Layer #5
         conv5a = tf.nn.conv2d(input=pool4,filter=w[10],strides=[1,1,1,1],padding="SAME")
-        conv5a = tf.nn.leaky_relu(conv5a)
+        conv5a = tf.nn.leaky_relu(conv5a,alpha=0.01)
         conv5b = tf.nn.conv2d(input=conv5a,filter=w[11],strides=[1,1,1,1],padding="SAME")
-        conv5b = tf.nn.leaky_relu(conv5b)
+        conv5b = tf.nn.leaky_relu(conv5b,alpha=0.01)
         conv5c = tf.nn.conv2d(input=pool4,filter=w[12],strides=[1,1,1,1],padding="SAME")
-        conv5c = tf.nn.leaky_relu(conv5c)
+        conv5c = tf.nn.leaky_relu(conv5c,alpha=0.01)
         res5 = tf.add(conv5b,conv5c)
         pool5 = tf.nn.max_pool(res5,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
         ''' !!!TODO!!!  (1, 4, 4, 256)   ->>>      Paper result size is (1, 5, 5, 256)'''
     
         # Convolutional Layer #6
         conv6a = tf.nn.conv2d(input=pool5,filter=w[13],strides=[1,1,1,1],padding="SAME")
-        conv6a = tf.nn.leaky_relu(conv6a)
+        conv6a = tf.nn.leaky_relu(conv6a,alpha=0.01)
         conv6b = tf.nn.conv2d(input=conv6a,filter=w[14],strides=[1,1,1,1],padding="SAME")
-        conv6b = tf.nn.leaky_relu(conv6b)
+        conv6b = tf.nn.leaky_relu(conv6b,alpha=0.01)
         pool6 = tf.nn.max_pool(conv6b,ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
         ''' !!!TODO!!!  (1, 2, 2, 256)   ->>>      Paper result size is (1, 3, 3, 256)'''
         
@@ -167,6 +168,7 @@ def build_graph():
             fc_layer_R = tf.reshape(fc_layer_R,[-1,4,128,4,4]) #[1,4,128,4,4]
             
             update_gate = tf.nn.conv3d(prev_s,w[17],strides=[1,1,1,1,1],padding="SAME")
+            #update_gate = tf.nn.leaky_relu(update_gate,alpha=0.01)
 
             update_gate = update_gate + fc_layer_U
             update_gate = tf.sigmoid(update_gate)
@@ -174,12 +176,14 @@ def build_graph():
             complement_update_gate = tf.subtract(tf.ones_like(update_gate),update_gate)
 
             reset_gate = tf.nn.conv3d(prev_s,w[18],strides=[1,1,1,1,1],padding="SAME")
+            #reset_gate = tf.nn.leaky_relu(reset_gate,alpha=0.01)
             reset_gate = reset_gate + fc_layer_R
             reset_gate = tf.sigmoid(reset_gate)
 
             rs = tf.multiply(reset_gate,prev_s) # Element-wise multiply
 
             tanh_reset = tf.nn.conv3d(rs,w[19],strides=[1,1,1,1,1],padding="SAME")
+            #tanh_reset = tf.nn.leaky_relu(tanh_reset,alpha=0.01)
             tanh_reset = tf.tanh(tanh_reset)
 
             gru_out = tf.add(
@@ -201,55 +205,60 @@ def build_graph():
         unpool7 = unpool(s)
 
         conv7a = tf.nn.conv3d(unpool7,w_decoder[0],strides=[1,1,1,1,1],padding="SAME")
-        conv7a = tf.nn.leaky_relu(conv7a)
+        conv7a = tf.nn.leaky_relu(conv7a,alpha=0.01)
 
         conv7b = tf.nn.conv3d(conv7a,w_decoder[1],strides=[1,1,1,1,1],padding="SAME")
-        conv7b = tf.nn.leaky_relu(conv7b)
+        conv7b = tf.nn.leaky_relu(conv7b,alpha=0.01)
         res7 = tf.add(unpool7,conv7b)
 
         unpool8 = unpool(res7)
 
         conv8a = tf.nn.conv3d(unpool8,w_decoder[2],strides=[1,1,1,1,1],padding="SAME")
-        conv8a = tf.nn.leaky_relu(conv8a)   
+        conv8a = tf.nn.leaky_relu(conv8a,alpha=0.01)   
 
         conv8b = tf.nn.conv3d(conv8a,w_decoder[3],strides=[1,1,1,1,1],padding="SAME")
-        conv8b = tf.nn.leaky_relu(conv8b)    
+        conv8b = tf.nn.leaky_relu(conv8b,alpha=0.01)    
         res8 = tf.add(unpool8,conv8b)
 
         unpool9 = unpool(res8)
 
         conv9a = tf.nn.conv3d(unpool9,w_decoder[4],strides=[1,1,1,1,1],padding="SAME")
-        conv9a = tf.nn.leaky_relu(conv9a)   
+        conv9a = tf.nn.leaky_relu(conv9a,alpha=0.01)   
 
         conv9b = tf.nn.conv3d(conv9a,w_decoder[5],strides=[1,1,1,1,1],padding="SAME")
-        conv9b = tf.nn.leaky_relu(conv9b)  
+        conv9b = tf.nn.leaky_relu(conv9b,alpha=0.01)  
 
         conv9c = tf.nn.conv3d(unpool9,w_decoder[6],strides=[1,1,1,1,1],padding="SAME")
 
         res9 = tf.add(conv9c,conv9b)
+        unpool10 = res9
 
-        conv10a = tf.nn.conv3d(res9,w_decoder[7],strides=[1,1,1,1,1],padding="SAME")
-        conv10a = tf.nn.leaky_relu(conv10a)  
+        conv10a = tf.nn.conv3d(unpool10,w_decoder[7],strides=[1,1,1,1,1],padding="SAME")
+        conv10a = tf.nn.leaky_relu(conv10a,alpha=0.01)  
         
         conv10b = tf.nn.conv3d(conv10a,w_decoder[8],strides=[1,1,1,1,1],padding="SAME")
-        conv10b = tf.nn.leaky_relu(conv10b)  
+        conv10b = tf.nn.leaky_relu(conv10b,alpha=0.01)  
 
-        conv10c = tf.nn.conv3d(conv10a,w_decoder[9],strides=[1,1,1,1,1],padding="SAME")
-        conv10c = tf.nn.leaky_relu(conv10c)  
+        conv10c = tf.nn.conv3d(conv10b,w_decoder[9],strides=[1,1,1,1,1],padding="SAME")
+        conv10c = tf.nn.leaky_relu(conv10c,alpha=0.01)  
 
-        res10 = tf.add(conv10c,conv10b)
+        unpool10_ = tf.ones([64,32],tf.float32)
+        unpool10_ = tf.matmul(tf.reshape(unpool10,[-1,64]),unpool10_)
+        unpool10_ = tf.reshape(unpool10_,[1,32,32,32,32])
+
+        res10 = tf.add(conv10c,unpool10_)
 
         conv11a = tf.nn.conv3d(res10,w_decoder[10],strides=[1,1,1,1,1],padding="SAME")
-        ##conv11a = tf.nn.leaky_relu(conv11a)  
-
+        conv11a = tf.nn.leaky_relu(conv11a,alpha=0.01)  
+        
         #conv11a = tf.contrib.layers.layer_norm(conv11a) #Norm layer
-        exp_x = tf.exp(conv11a, name="Decoder_Output")
-        sum_exp_x = tf.reduce_sum(exp_x,axis=4,keepdims=True)
-        prediction = tf.divide(exp_x,sum_exp_x)
 
-    loss_ = paper_loss(conv11a,Y) #conv11a is final prediction. Y is ground truth.
+    exp_x = tf.exp(conv11a)
+    sum_exp_x = tf.reduce_sum(exp_x,axis=4,keepdims=True)
 
-    return loss_, prediction
+    loss_ = paper_loss(conv11a,sum_exp_x,Y) #conv11a is final prediction. Y is ground truth.
+    pred = exp_x / sum_exp_x
+    return loss_, pred
 
  
 
@@ -267,9 +276,9 @@ def berkan_loss(x,y):
         l = tf.nn.softmax_cross_entropy_with_logits_v2(logits=x,labels=y)
     return tf.reduce_sum(l)
 
-def paper_loss(y,yhat):
-    exp_x = tf.exp(y)
-    sum_exp_x = tf.reduce_sum(exp_x,axis=4,keepdims=True)
+def paper_loss(y,sum_exp_x,yhat):
+    #exp_x = tf.exp(y)
+    #sum_exp_x = tf.reduce_sum(exp_x,axis=4,keepdims=True)
     log_x = tf.log(sum_exp_x)
     #log_x = tf.log(exp_x)
     #sum_log_x = tf.reduce_sum(log_x)
@@ -311,34 +320,17 @@ def unpool(value):
     return out
 
 
-#forward_pass = encoder_gru()
-
-loss_,obj = build_graph()
-
-#decoder_pass = decoder()
-
-#logits = decoder_pass
-
-# Define loss and optimizer
-#loss_op = tf.nn.softmax_cross_entropy_with_logits_v2(logits=obj,labels=Y)
-#sample = decoder_pass
+loss_, pred_ = build_graph()
 
 # Optimization
-optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss_)
-#grads_vars = optimizer.compute_gradients(loss_)
-#clipped_gradients = []
-#for grad,var in grads_vars:
-#    clipped_gradients.append(
-#        (tf.clip_by_value(grad,5.0,-5.0),var)) # 1 is max_gradient_norm)
+optimizer = tf.train.AdamOptimizer(1e-4)
+grads_vars = optimizer.compute_gradients(loss_)
+clipped_gradients = []
+for grad,var in grads_vars:
+    clipped_gradients.append(
+        (tf.clip_by_value(grad,5.0,-5.0),var)) # 1 is max_gradient_norm)
 
-#updates = optimizer.apply_gradients(clipped_gradients)
-# Calculate and clip gradients
-#params = tf.trainable_variables()
-#gradients = tf.gradients(loss_)
-#clipped_gradients, _ = tf.clip_by_global_norm(
-#   gradients, tf.constant(5,name="max_gradient_norm",dtype=tf.float32)) # 1 is max_gradient_norm
-#update_step = optimizer.apply_gradients(
-#    zip(clipped_gradients, params))
+updates = optimizer.apply_gradients(clipped_gradients)
 
 
 if __name__=="__main__":
@@ -347,9 +339,8 @@ if __name__=="__main__":
     # Start training
     with tf.Session() as sess:
 
-        #Debugger   
-        #sess = tf_debug.TensorBoardDebugWrapperSession(sess, "Berkan-MacBook-Pro.local:4334")
 
+        #sess = tf_debug.TensorBoardDebugWrapperSession(sess, "Berkan-MacBook-Pro.local:4334")
         # Run the initializer
         sess.run(init)
 
@@ -367,10 +358,10 @@ if __name__=="__main__":
         x_train = dataset.train_data()
         y_train = dataset.train_labels()
 
-        pbar = tqdm(total=dataset.TOTAL_SIZE)
-        #pbar = tqdm(total=10)
-        while(x_train!=[] and y_train!=[]):
-        #while(iter<10): # 5000 iterations
+        #pbar = tqdm(total=dataset.TOTAL_SIZE)
+        pbar = tqdm(total=10)
+        #while(x_train!=[] and y_train!=[]):
+        while(iter<10): # 5000 iterations
             for image_hash in x_train.keys():
                 iter+=1
 
@@ -393,7 +384,7 @@ if __name__=="__main__":
                 batch_voxel[0,:,:,:,0]= vox < 1
                 batch_voxel[0,:,:,:,1]= vox
 
-                l,u,o = sess.run([loss_, optimizer, obj], feed_dict={X: ims, Y: batch_voxel})
+                l,u,o = sess.run([loss_, updates, pred_], feed_dict={X: ims, Y: batch_voxel})
 
                 print("OBJECT: " + str(iter)+" LOSS: "+str(l))
                 with open("log.txt", "a") as myfile:
@@ -413,10 +404,9 @@ if __name__=="__main__":
 
 
                     
-            x_train = dataset.train_data()
-            y_train = dataset.train_labels()
+            #x_train = dataset.train_data()
+            #y_train = dataset.train_labels()
 
         pbar.close()
         print("Finished!")
-
 
